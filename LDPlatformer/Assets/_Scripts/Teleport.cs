52f86a83e95883bcb485a0576fa9c41fa03222d9
Andrew Castillo <valax321@gmail.com>
2016-12-12 13:21:06 +1100

    BUGS

--- LDPlatformer/Assets/_Scripts/Teleport.cs
+++ LDPlatformer/Assets/_Scripts/Teleport.cs
@@ -7,9 +7,17 @@ public class Teleport : MonoBehaviour {
     [SerializeField]
     GameObject[] spawnPoints = new GameObject[2];
     [SerializeField]
+    GameObject[] backgroundScenery = new GameObject[2];
+    [SerializeField]
     GameObject player;
     int currentRoom = 0;
     public bool canTeleport = true;
+
+    void Awake()
+    {
+        backgroundScenery[currentRoom].SetActive(true);
+        backgroundScenery[1 - currentRoom].SetActive(false);
+    }
 
 	void Update () {
         if (Input.GetKeyDown(KeyCode.Q) && canTeleport)
@@ -20,10 +28,25 @@ public class Teleport : MonoBehaviour {
 
     void TeleportToRoom()
     {
+        //Todo: check if we would warp into something when teleporting - do a capsuletrace at the offset and see if it hits anything.
+
         int roomNumber = 1 - currentRoom;
         Vector3 relativeLocation = spawnPoints[currentRoom].transform.InverseTransformPoint(player.transform.position);
-        player.transform.position = spawnPoints[roomNumber].transform.position + relativeLocation;
+        Vector3 rotDiff = spawnPoints[0].transform.eulerAngles - spawnPoints[1].transform.eulerAngles;
+        Debug.Log(rotDiff);        
+        Vector3 tempOs = spawnPoints[roomNumber].transform.position + relativeLocation;
+        player.transform.position = RotatePointAroundPivot(tempOs, spawnPoints[roomNumber].transform.position, rotDiff);
+        backgroundScenery[roomNumber].SetActive(true);
+        backgroundScenery[currentRoom].SetActive(false);
         currentRoom = roomNumber;
+    }
+
+    Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles)
+    {
+        var dir = point - pivot;
+        dir = Quaternion.Euler(angles) * dir;
+        point = dir + pivot;
+        return point;
     }
 
     void OnCollisionEnter()
